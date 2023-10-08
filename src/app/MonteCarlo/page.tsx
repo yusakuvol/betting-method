2c0ba784b7c3fc12b53b2c190e4d78cbe74a2948
yusakuvol <yoursakuyu@gmail.com>
2023-10-08 09:49:13 +0900

    add Monte Carlo

---
+++ src/app/MonteCarlo/page.tsx
@@ -0,0 +1,12 @@
+export default function Martingale() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-6xl font-bold text-center">モンテカルロ法</h1>
+      <div className="flex flex-col items-center justify-center">
+        <p className="text-2xl text-center">
+          モンテカルロ法は、カジノゲームのBET方法。
+        </p>
+      </div>
+    </main>
+  )
+}
