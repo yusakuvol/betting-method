9313cb8020fddd3ff3ea9048b916a604d30f737d
yusakuvol <yoursakuyu@gmail.com>
2023-10-08 09:41:54 +0900

    update words

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-6xl font-bold text-center">Next.js ISR PRA</h1>
+      <h1 className="text-6xl font-bold text-center">カジノBET</h1>
       <div className="flex flex-col items-center justify-center">
         <p className="text-2xl text-center">
-          This is a demo page with Next.js.
+          カジノゲームのBETを管理するためのサイトです。
+        </p>
+      </div>
+      <div className="flex flex-col items-center justify-center">
+        <p className="text-2xl text-center">
+          このサイトは、カジノゲームのBETを管理するためのサイトです。
         </p>
       </div>
     </main>
